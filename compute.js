14371bf936154d8612ff37d8c27db97a46e4a3ea
faslm <fatima27579@gmail.com>
2019-11-10 04:19:24 -0800

    add js files

---
+++ compute.js
@@ -0,0 +1,16 @@
+export function add(x , y) {
+    return x + y;
+}
+
+export function sub(x , y) {
+    return x - y;
+}
+
+export function multi(x , y) {
+    return x * y;
+}
+
+
+export function divi(x , y) {
+    return x / y;
+}
